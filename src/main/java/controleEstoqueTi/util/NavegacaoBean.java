e2c091aea2f678aa1fd1443dfff6ba1026d1da68
Claudio Ramos de Oliveira <claudio.ramosti@yahoo.com.br>
2017-04-18 17:22:00 -0300

    Acrescetou classe de navegacao centralizando outcomes e configurando tbm no faces-config os casos de navegacao

---
+++ src/main/java/controleEstoqueTi/util/NavegacaoBean.java
@@ -0,0 +1,38 @@
+package controleEstoqueTi.util;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+public class NavegacaoBean  implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7523464918984226240L;
+
+	private final String outcome_Equipamentos = "Crud_Equipamento";
+	
+	private final String outcome_TiposEquipamentos = "Crud_Tipo_Equipamento";
+
+	public String iniciarEquipamentos(){
+		return outcome_Equipamentos;
+	}
+	
+	public String iniciarTiposEquipamentos(){
+		return outcome_TiposEquipamentos;
+	}
+
+	public String getOutcome_Equipamentos() {
+		return outcome_Equipamentos;
+	}
+
+	public String getOutcome_TiposEquipamentos() {
+		return outcome_TiposEquipamentos;
+	}
+
+	
+	
+}
